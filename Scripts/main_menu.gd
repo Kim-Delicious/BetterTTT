6c96447376956cdaf8d6a387b2b3724b07024be6
Kimber-Delicious <clifton.kimber@gmail.com>
2024-10-21 04:33:44 -0600

    Made lot's of progress making the Main Menu. It's nearly done, looks-wise. Just need to make it functional!

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,35 +1,30 @@
-extends Node2D
+extends Control
 
-@onready var main_node: Control = $VBoxContainer/Main
-@onready var singleplayer_node: Control = $VBoxContainer/Singleplayer
-@onready var multiplayer_node: Control = $VBoxContainer/Multiplayer
-@onready var settings_node: Control = $VBoxContainer/Settings
-@onready var map_maker_node: Control = $VBoxContainer/MapMaker
+@onready var main: VBoxContainer = $MainMenu
+
+@onready var settings: Control = $SettingsMenu
+@onready var selection: Control = $SelectionMenu
 
 
 func _set_all_menues_invisible() -> void:
-	for menu in get_child(0).get_children():
-		menu.visible = false
+	main.hide()
+	settings.hide()
+	selection.hide()
 
 
 #region Main
-func _on_single_player_pressed() -> void:
-	_set_all_menues_invisible()
-	singleplayer_node.visible = true
+
+func _on_tutorial_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Levels/tutorial.tscn")
 
 
-
-func _on_multiplayer_pressed() -> void:
-	_set_all_menues_invisible()
-	multiplayer_node.visible = true
-
-func _on_mapmaker_pressed() -> void:
-	_set_all_menues_invisible()
-	map_maker_node.visible = true
+func _on_play_pressed() -> void:
+	main.hide()
+	selection.show()
 
 func _on_settings_pressed() -> void:
-	_set_all_menues_invisible()
-	settings_node.visible = true
+	main.hide()
+	settings.show()
 
 
 func _on_quit_button_pressed() -> void:
@@ -37,45 +32,22 @@ func _on_quit_button_pressed() -> void:
 	
 #endregion
 
-#region Singleplayer
-
-func _on_continue_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_new_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_load_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_back_pressed() -> void:
-	_set_all_menues_invisible()
-	main_node.visible = true
-
-#endregion
-
-#region MapMaker
-func _on_open_map_pressed() -> void:
-	pass # Replace with function body.
+func _on_back_button_pressed() -> void:
+	settings.hide()
+	selection.hide()
+	main.show()
+	
+func selection_next() -> void:
+	selection.get_child(0).hide()
+	selection.get_child(1).show()
+	pass
+	
+func selection_back() -> void:
+	selection.get_child(0).show()
+	selection.get_child(1).hide()
+	pass
 
 
-func _on_new_map_pressed() -> void:
-	pass # Replace with function body.
-#endregion
-#region Multiplayer
 
-func _on_online_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_local_pressed() -> void:
+func _on_final_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Levels/local_game.tscn")
-#endregion
-
-
-#region Settings
-
-#endregion
